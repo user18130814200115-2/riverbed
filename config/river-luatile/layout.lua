6ed03c8cd172d81ac61deb7247770a7aced0f693
Thijs Iris Wester <wester.t@protonmail.com>
2024-08-27 19:06:18 +0200

    Switched waybar to Dock mode, Added maximize command to luatile
    Having waybar in Dock mode, rather than in overlay allows for
    clickable events.  It also means that waybar requests space from
    river for the whole top area. I mitigate this in luatile by
    simply ignoring this and offsetting the difference.
    
    Luatile layout also got a maximize command, which I find slightly
    nicer than fullscreen.

--- config/river-luatile/layout.lua
+++ config/river-luatile/layout.lua
@@ -1,29 +1,47 @@
 local main_ratio = 0.60
 local gaps = 10
-local offset = 60
+local bar_height = 48
+local maximize = false
+
+local offset = bar_height + 2 * gaps
 
 function handle_layout(args)
 	local retval = {}
-	if args.count == 1 then
-		table.insert(retval, { gaps, gaps, (args.width - gaps * 3) * main_ratio, args.height - gaps * 2 })
-	elseif args.count > 1 then
-		local main_w = (args.width - gaps * 3) * main_ratio
-		local side_w = (args.width - gaps * 3) - main_w
-		local main_h = args.height - gaps * 2
-		local side_h = (args.height - gaps - offset) / (args.count - 1) - gaps
-		table.insert(retval, {
-			gaps,
-			gaps,
-			main_w,
-			main_h,
-		})
-		for i = 0, (args.count - 2) do
+
+	if maximize then
+		table.insert(retval, { gaps, gaps - offset, (args.width - gaps * 2), args.height - gaps * 2 + offset })
+		if args.count > 1 then
+			for i = 0, (args.count - 2) do
+				table.insert(retval, {
+					0,
+					0,
+					0,
+					0,
+				})
+			end
+		end
+	else
+		if args.count == 1 then
+			table.insert(retval, { gaps, gaps - offset, (args.width - gaps * 3) * main_ratio, args.height - gaps * 2 + offset })
+		elseif args.count > 1 then
+			local main_w = (args.width - gaps * 3) * main_ratio
+			local side_w = (args.width - gaps * 3) - main_w
+			local main_h = args.height - gaps * 2 + offset
+			local side_h = (args.height - gaps) / (args.count - 1)
 			table.insert(retval, {
-				main_w + gaps * 2,
-				offset + gaps + i * (side_h + gaps),
-				side_w,
-				side_h,
+				gaps,
+				gaps - offset,
+				main_w,
+				main_h,
 			})
+			for i = 0, (args.count - 2) do
+				table.insert(retval, {
+					main_w + gaps * 2,
+					i * (side_h),
+					side_w,
+					side_h,
+				})
+			end
 		end
 	end
 	return retval
@@ -31,4 +49,8 @@ end
 
 function handle_metadata(args)
 	return { name = "runoff" }
+end
+
+function toggle_maximize()
+	maximize = not maximize
 end
