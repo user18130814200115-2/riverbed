103efb99d134fe753582a6b200ebc65bd231d2fe
Thijs Iris Wester <wester.t@protonmail.com>
2024-09-02 18:28:09 +0200

    New layout: dual

--- config/river-luatile/layout.lua
+++ config/river-luatile/layout.lua
@@ -1,31 +1,24 @@
--- These lines will be automatically updated by river/init
---local gaps = 10
---local main_ratio = 0.60
---local display_height = 1080
--- END
-
-dofile('/home/user/.config/river-luatile/riverbed-autogen.lua')
-
 local maximize = false
-local bar_height = 0
+
+function update_variables()
+	dofile('/home/user/.config/river-luatile/riverbed-autogen.lua')
+end
 
 function toggle_maximize()
 	maximize = not maximize
 end
 
-function set_display_height(input)
-	display_height = input
-end
+update_variables()
+
 
 function handle_layout(args)
-
+	local retval = {}
 	if args.height < display_height then
 		bar_height = display_height - args.height
 	else
 		bar_height = 0
 	end
 
-	local retval = {}
 	if maximize then
 		table.insert(retval, {
 			gaps,
@@ -36,30 +29,53 @@ function handle_layout(args)
 		for i = 0, (args.count - 2) do
 			table.insert(retval, {0,display_height,0,0})
 		end
-	else
-		if args.count == 1 then
+		return retval
+	end
+
+	if layout == 'runoff' then
+		local main_w = (args.width - gaps * 3) * main_ratio
+		local side_w = (args.width - gaps * 3) - main_w
+		local main_h = args.height + bar_height - gaps * 2
+		local side_h = (args.height - gaps) / (args.count - 1) - gaps
+		table.insert(retval, {
+			gaps,
+			gaps - bar_height,
+			main_w,
+			main_h,
+		})
+		for i = 0, (args.count - 2) do
 			table.insert(retval, {
-				gaps,
-				gaps - bar_height,
-				(args.width - gaps * 3) * main_ratio,
-				args.height + bar_height - gaps * 2
+				main_w + gaps * 2,
+				gaps + i * (side_h + gaps),
+				side_w,
+				side_h,
 			})
-		elseif args.count > 1 then
-			local main_w = (args.width - gaps * 3) * main_ratio
-			local side_w = (args.width - gaps * 3) - main_w
-			local main_h = args.height + bar_height - gaps * 2
-			local side_h = (args.height - gaps) / (args.count - 1) - gaps
-			table.insert(retval, {
-				gaps,
-				gaps - bar_height,
-				main_w,
-				main_h,
-			})
-			for i = 0, (args.count - 2) do
+		end
+	elseif layout == 'dual' then
+		local main_w = (args.width - gaps * 3) * main_ratio
+		local side_w = ( (args.width - gaps * 3) - main_w ) / 2
+		local main_h = args.height - gaps * 2
+		table.insert(retval, {
+			gaps + side_w,
+			gaps,
+			main_w,
+			main_h,
+		})
+		for i = 0, (args.count - 2) do
+			if i % 2 == 0 then
+				local side_h = (args.height - gaps + bar_height) / (math.floor(args.count / 2)) - gaps
 				table.insert(retval, {
-					main_w + gaps * 2,
-					gaps + i * (side_h + gaps),
+					main_w + side_w + gaps * 2,
+					gaps + math.floor(i/2) * (side_h + gaps) - bar_height,
 					side_w,
+					side_h,
+				})
+			else
+				local side_h = (args.height - gaps + bar_height) / (math.floor((args.count - 1 )/ 2)) - gaps
+				table.insert(retval, {
+					gaps,
+					gaps + math.floor(i/2) * (side_h + gaps) - bar_height,
+					side_w - gaps,
 					side_h,
 				})
 			end
